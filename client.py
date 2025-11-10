91ee6792d354b94c3cc5690442f6ea429afb27ef
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-11-10 20:36:11 +0100

    starter of client

--- client.py
+++ client.py
@@ -5,3 +5,17 @@ import time
 import sys
 import base64
 from pathlib import Path
+
+def recv_json_line(sock):
+    buff = b""
+    while True:
+        ch = sock.recv(1)
+        if not ch:
+            return None
+        buff += ch
+        if ch == b"\n":
+            break
+    try:
+        return json.loads(buff.decode("utf-8"))
+    except Exception:
+        return {"raw": buff.decode("utf-8", errors="ignore")}
